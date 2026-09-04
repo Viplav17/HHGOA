d4e50e0336623c3b911801d1db5785caa3d3d975
nagpalvv <viplavnagpal1704@gmail.com>
2026-09-05 01:37:22 +0530

    Added function to add a new block in the chain

--- Blockchain/Backend/Core/BlockChain.py
+++ Blockchain/Backend/Core/BlockChain.py
@@ -1,6 +1,9 @@
 from Blockchain.Backend.Core.Block import Block
 from Blockchain.Backend.Core.BlockHeader import BlockHeader
+from Blockchain.Backend.util.util import hash256
 import time
+import json as js
+
 
 ZERO_HASH = "0" * 64
 
@@ -10,11 +13,45 @@ class BlockChain:
 
     def Genesis_Block(self):
         Gen_Block_Head = BlockHeader(
-            0, ZERO_HASH, ZERO_HASH, 0, time.time()
+            0,
+            ZERO_HASH,
+            ZERO_HASH,
+            0,
+            time.time()
             )
         
         Gen_Block = Block(
-            0, 0, Gen_Block_Head
+            0,
+            0,
+            Gen_Block_Head
             )
         
         return Gen_Block
+
+    def Add_Block(self, data: list):
+        prev_block = self.chain[-1]
+        prev_hash = prev_block.block_hash
+
+        Encoded_data = js.dumps(data, sort_keys=True)
+        merkle_root = hash256(Encoded_data).hex()
+
+        Block_Header = BlockHeader(
+            1,
+            prev_hash,
+            merkle_root,
+            0,
+            time.time()
+            )
+        
+        New_Block = Block(
+            len(self.chain),
+            len(Encoded_data),
+            Block_Header,
+            data
+            )
+
+        self.chain.append(New_Block)
+        return New_Block
+
+    def Verify_Block(self, Block):
+        
