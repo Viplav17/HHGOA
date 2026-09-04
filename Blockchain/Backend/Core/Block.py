0720dcb7da33fb88f68e782856ae4a0a32d6854a
nagpalvv <viplavnagpal1704@gmail.com>
2026-09-05 02:45:43 +0530

    docs: add block class documentation

--- Blockchain/Backend/Core/Block.py
+++ Blockchain/Backend/Core/Block.py
@@ -7,7 +7,7 @@ class Block:
         Blocksize -> int: Size of the encoded data in bytes.
         BlockHeader -> BlockHeader: The header object which contains Metadata (eg. Previous Block Hash, Merkle Root).
         Data -> list: A list of the data matched online (e.g., discovered social media URLs and face hashes).
-        block_hash (str): The unique SHA-256 identifier for this block, generated from its header. 
+        Block_Hash (str): The unique SHA-256 identifier for this block, generated from its header. 
     """
     def __init__(self, Height, Blocksize, BlockHeader, Data = None):
         self.Height = Height
