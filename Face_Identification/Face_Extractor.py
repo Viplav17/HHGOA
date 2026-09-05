2b0b2337e2b6df68169b74429d6f48ede31d3887
Romila Gholse <romilagholse@gmail.com>
2026-09-05 16:56:33 +0530

    Face detection and encoding added

--- Face_Identification/Face_Extractor.py
+++ Face_Identification/Face_Extractor.py
@@ -1,5 +1,4 @@
 import cv2
-import json
 from deepface import DeepFace
 
 MODEL_NAME = "ArcFace" 
@@ -11,7 +10,7 @@ def extract_and_prepare(image_path: str):
     """
     img = cv2.imread(image_path)
     if img is None:
-        raise FileNotFoundError(f"Cannot open '{image_path}'") #error line if image does not open
+        raise FileNotFoundError(f"Cannot open '{image_path}'") 
 
     results = DeepFace.represent(
         img_path=img,
@@ -22,38 +21,10 @@ def extract_and_prepare(image_path: str):
     )
 
     face = results[0] 
-    area = face["facial_area"]
     embedding = face["embedding"] 
-    """""""""""
 
-    Up until here was only for getting the vector needed that is unique to each face
 
-    Next, the cropped image that only includes the face and no other background
-
-    """""""""""
-
-    x, y, w, h = area["x"], area["y"], area["w"], area["h"]
-    face_crop = img[y:y + h, x:x + w]
     
-    
-    cv2.imwrite("face_crop.jpg", face_crop)
-
-    payload = {
-        "model": MODEL_NAME,
-        "detector_backend": DETECTOR,
-        "embedding": embedding,
-        "vector_dim": len(embedding)
-    }        
-
-
-
-    with open("face_data.json", "w") as f:
-        json.dump(payload, f, indent=2)
-
     # Returns the actual variables
-    return face_crop, payload
+    return embedding
 
-if __name__ == "__main__":
-    crop, data = extract_and_prepare("Face Identification/img1.png")
-    print(f"Test successful. Extracted {data['vector_dim']}-dim vector.")
-    print(data, crop)
