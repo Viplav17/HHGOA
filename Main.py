f30f915987674227a250e1ed584f8218669fdba2
nagpalvv <viplavnagpal1704@gmail.com>
2026-09-05 21:15:11 +0530

    Created Main.py to contain the main pipeline

---
+++ Main.py
@@ -0,0 +1,7 @@
+import os
+import json
+import time
+
+from Face_Identification.Face_Extractor import  Detect_Encode_Face
+from Searching.Search import Search
+from Blockchain.Backend.Core.BlockChain import BlockChain
